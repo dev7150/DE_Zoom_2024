0f7742989031d48e860b2b107341a9b2657d86b2
dev7150 <devashish16@gmail.com>
2024-03-03 03:27:37 +0000

    mage pipeline

---
+++ final_project/magic-zoomcamp/data_exporters/bq.sql
@@ -0,0 +1,2 @@
+-- Docs: https://docs.mage.ai/guides/sql-blocks
+Select * from {{df_1}}
---
+++ final_project/magic-zoomcamp/data_loaders/load_csv.py
@@ -0,0 +1,29 @@
+if 'data_loader' not in globals():
+    from mage_ai.data_preparation.decorators import data_loader
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+import dlt
+import pandas as pd
+
+@data_loader
+def load_data(*args, **kwargs):
+    """
+    Template code for loading data from any source.
+
+    Returns:
+        Anything (e.g. data frame, dictionary, array, int, str, etc.)
+    """
+    # Specify your data loading logic here
+    
+    df = pd.read_csv('all_foot.csv',parse_dates=['SensingDateTime(Hour)'])
+    print(df.columns)
+    return df
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
